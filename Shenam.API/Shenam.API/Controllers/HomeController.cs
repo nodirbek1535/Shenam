e34791a017be3730f5d6f6b0c3c27be68540698a
Nodirbek Nasrullayev <Sulaymonovnasim@gmail.com>
2025-02-14 00:38:43 +0500

    INFRA: Initialize API Project

---
+++ Shenam.API/Shenam.API/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿//===============================================================
+//NODIRBEKNING MOHIRDEV PLATFORMASIDA ORGANGAN API SINOV LOYIHASI
+//===============================================================
+using Microsoft.AspNetCore.Mvc;
+using RESTFulSense.Controllers;
+
+namespace Shenam.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HomeController : RESTFulController
+    {
+        [HttpGet]
+        public ActionResult<string> Get() =>
+            Ok("hammasi ishlayapti");
+    }
+}
---
+++ Shenam.API/Shenam.API/Startup.cs
@@ -0,0 +1,62 @@
+//===============================================================
+//NODIRBEKNING MOHIRDEV PLATFORMASIDA ORGANGAN API SINOV LOYIHASI
+//===============================================================
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using System;
+
+namespace Shenam.API
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration) =>
+            Configuration = configuration;
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            var apiinfo = new OpenApiInfo
+            {
+                Title = "Shenam.API",
+                Version = "v1"
+            };
+
+            services.AddControllers();
+
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc(
+                    name:"v1", 
+                    info:apiinfo);
+            });
+        }
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment envirovment)
+        {
+            if (envirovment.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+
+                app.UseSwaggerUI(options =>
+                {
+                    options.SwaggerEndpoint(
+                    url: "/swagger/v1/swagger.json",
+                    name: "Shenam.API v1");
+                });                
+            }
+
+            app.UseHttpsRedirection();
+            app.UseRouting();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>            
+                endpoints.MapControllers());
+        }
+    }
+}
