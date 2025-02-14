38eaea255e850967eac446b576487b704f9aae97
Nodirbek Nasrullayev <Sulaymonovnasim@gmail.com>
2025-02-14 22:08:36 +0500

    INFRA: Initialize Unit Test Project

---
+++ Shenam.API/Shenam.Api.Tests.Unit/DeleteME.cs
@@ -0,0 +1,12 @@
+﻿//===============================================================
+//NODIRBEKNING MOHIRDEV PLATFORMASIDA ORGANGAN API SINOV LOYIHASI
+//===============================================================
+using Xunit;
+namespace Shenam.Api.Tests.Unit
+{
+    public class DeleteME
+    {
+        [Fact]
+        public void ShouldBeTrue() => Assert.True(true);
+    }
+}
