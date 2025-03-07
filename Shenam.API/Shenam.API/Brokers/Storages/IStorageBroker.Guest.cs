43169f2974238bfc305657cb099f99067c3ff1d2
Nodirbek Nasrullayev <Sulaymonovnasim@gmail.com>
2025-03-07 18:12:43 +0500

    BROKERS: Insert Guest

---
+++ Shenam.API/Shenam.API/Brokers/Storages/IStorageBroker.Guest.cs
@@ -0,0 +1,19 @@
+﻿//===============================================================
+//NODIRBEKNING MOHIRDEV PLATFORMASIDA ORGANGAN API SINOV LOYIHASI
+//===============================================================
+
+using Microsoft.EntityFrameworkCore.Storage;
+using Shenam.API.Models.Foundation.Guests;
+using System.Threading.Tasks;
+
+namespace Shenam.API.Brokers.Storages
+{
+    public partial interface IStorageBroker
+    {
+        //CREATE
+        //READ
+        //UPDATE
+        //DELETE
+        ValueTask<Guest> InsertGuestAsync(Guest guest);
+    }
+}
--- Shenam.API/Shenam.API/Brokers/Storages/StorageBroker.Guests.cs
+++ Shenam.API/Shenam.API/Brokers/Storages/StorageBroker.Guests.cs
@@ -3,12 +3,25 @@
 //===============================================================
 
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
 using Shenam.API.Models.Foundation.Guests;
+using System.Threading.Tasks;
 
 namespace Shenam.API.Brokers.Storages
 {
     public partial class StorageBroker
     {
         public DbSet<Guest> Guests { get; set; }
+
+        public async ValueTask<Guest> InsertGuestAsync(Guest guest)
+        {
+            using var broker = new StorageBroker(this.configuration);
+
+            EntityEntry<Guest> guestEntityEntry = 
+                await broker.Guests.AddAsync(guest);
+            await broker.SaveChangesAsync();
+
+            return guestEntityEntry.Entity;
+        }
     }
 }
