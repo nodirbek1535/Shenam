9a0214029afc02d03d596d41deab4ef5248ee7d3
Nodirbek Nasrullayev <Sulaymonovnasim@gmail.com>
2025-04-08 17:31:40 +0500

    BROKERS: Implement Logging

---
+++ Shenam.API/Shenam.API/Brokers/loggings/LoggingBroker.cs
@@ -0,0 +1,23 @@
+﻿//===============================================================
+//NODIRBEKNING MOHIRDEV PLATFORMASIDA ORGANGAN API SINOV LOYIHASI
+//===============================================================
+
+using System;
+using Microsoft.Extensions.Logging;
+
+namespace Shenam.API.Brokers.loggings
+{
+    public class LoggingBroker : ILoggingBroker
+    {
+        private readonly ILogger<LoggingBroker> logger;
+
+        public LoggingBroker(ILogger<LoggingBroker> logger) =>
+            this.logger = logger;
+        
+        public void LogError(Exception exception)=>
+            this.logger.LogError(exception, exception.Message);
+
+        public void LogCritical(Exception exception)=>
+            this.logger.LogCritical(exception, exception.Message);
+    }
+}
