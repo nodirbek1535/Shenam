528489c135dec2fe7d3d0ccea393b6bba64b75a2
Nodirbek Nasrullayev <Sulaymonovnasim@gmail.com>
2025-09-18 14:48:12 +0500

    CODE RUB: Implement TryCatch & Proper Validation

---
+++ Shenam.API/Shenam.API/Services/Foundations/Guests/GuestService.Exceptions.cs
@@ -0,0 +1,38 @@
+﻿//===============================================================
+//NODIRBEKNING MOHIRDEV PLATFORMASIDA ORGANGAN API SINOV LOYIHASI
+//===============================================================
+
+using System.Threading.Tasks;
+using Shenam.API.Models.Foundation.Guests;
+using Shenam.API.Models.Foundation.Guests.Exceptions;
+using Xeptions;
+
+namespace Shenam.API.Services.Foundations.Guests
+{
+    public partial class GuestService
+    {
+        private delegate ValueTask<Guest> ReturnningGuestFunction();
+
+        private async ValueTask<Guest> TryCatch(ReturnningGuestFunction returnningGuestFunction)
+        {
+            try
+            {
+                return await returnningGuestFunction();
+            }
+            catch (NullGuestException nullGuestException)
+            {
+                throw CreateAndLogValidationException(nullGuestException);
+            }
+        }
+
+        private GuestValidationException CreateAndLogValidationException(Xeption exception)
+        {
+            var guestValidationException =
+                new GuestValidationException(exception);
+
+            this.loggingBroker.LogError(guestValidationException);
+
+            return guestValidationException;
+        }
+    }
+}
--- Shenam.API/Shenam.API/Services/Foundations/Guests/GuestService.cs
+++ Shenam.API/Shenam.API/Services/Foundations/Guests/GuestService.cs
@@ -2,16 +2,14 @@
 //NODIRBEKNING MOHIRDEV PLATFORMASIDA ORGANGAN API SINOV LOYIHASI
 //===============================================================
 
-using System;
 using System.Threading.Tasks;
 using Shenam.API.Brokers.loggings;
 using Shenam.API.Brokers.Storages;
 using Shenam.API.Models.Foundation.Guests;
-using Shenam.API.Models.Foundation.Guests.Exceptions;
 
 namespace Shenam.API.Services.Foundations.Guests
 {
-    public class GuestService : IGuestService
+    public partial class  GuestService : IGuestService
     {
         private readonly IStorageBroker storageBroker;
         private readonly ILoggingBroker loggingBroker;
@@ -24,28 +22,12 @@ namespace Shenam.API.Services.Foundations.Guests
             this.loggingBroker = loggingBroker;
         }
 
-        public async ValueTask<Guest> AddGuestAsync(Guest guest)
+        public ValueTask<Guest> AddGuestAsync(Guest guest) =>
+        TryCatch(async () =>
         {
-            try
-            {
-                if (guest is null)
-                {
-                    throw new NullGuestException();
-                }
-
-
-                return await this.storageBroker.InsertGuestAsync(guest);
-            }
-            catch (NullGuestException nullGuestException)
-            {
-                var guestValidationException =
-                    new GuestValidationException(nullGuestException);
-
-                this.loggingBroker.LogError(guestValidationException);
-
-                throw guestValidationException;
-            }
-        }
- 
+            ValidateGuestNotNull(guest);
+              
+            return await this.storageBroker.InsertGuestAsync(guest);
+        });
     }
 }
