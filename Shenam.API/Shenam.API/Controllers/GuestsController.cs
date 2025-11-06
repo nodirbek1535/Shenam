679fea26ec2b0a1438df065e3fa6ad01a4e4a183
Nodirbek Nasrullayev <nasrullayevnodirbek1535@gmail.com>
2025-11-06 11:16:45 +0500

    CONFLICT: Fix Errors

---
+++ Shenam.API/Shenam.API/Controllers/GuestsController.cs
@@ -0,0 +1,28 @@
+﻿//===============================================================
+//NODIRBEKNING MOHIRDEV PLATFORMASIDA ORGANGAN API SINOV LOYIHASI
+//===============================================================
+
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Shenam.API.Models.Foundation.Guests;
+using Shenam.API.Services.Foundations.Guests;
+
+namespace Shenam.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class GuestsController : Controller
+    {
+        private readonly IGuestService guestService;
+        public GuestsController(IGuestService guestService)
+        {
+            this.guestService = guestService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PostGuest(Guest guest)
+        {
+            return Ok(this.guestService.AddGuestAsync(guest));
+        }
+    }
+}
