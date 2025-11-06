5addf7961dacac7c8fca40baa3c6ac71d5cd5e6c
Nodirbek Nasrullayev <nasrullayevnodirbek1535@gmail.com>
2025-11-06 23:39:58 +0500

    CONTROLLERS: Post Guest

---
+++ Shenam.API/Shenam.API/Controllers/GuestsController.cs
@@ -0,0 +1,54 @@
+﻿//===============================================================
+//NODIRBEKNING MOHIRDEV PLATFORMASIDA ORGANGAN API SINOV LOYIHASI
+//===============================================================
+
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RESTFulSense.Controllers;
+using Shenam.API.Models.Foundation.Guests;
+using Shenam.API.Models.Foundation.Guests.Exceptions;
+using Shenam.API.Services.Foundations.Guests;
+
+namespace Shenam.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class GuestsController : RESTFulController
+    {
+        private readonly IGuestService guestService;
+
+        public GuestsController(IGuestService guestService)
+        {
+            this.guestService = guestService;
+        }
+
+        [HttpPost]
+        public async ValueTask<ActionResult<Guest>> PostGuestAsync(Guest guest)
+        {
+            try
+            {
+                Guest postedGuest = await this.guestService.AddGuestAsync(guest);
+
+                return Created(postedGuest);
+            }
+            catch (GuestValidationException guestValidationException)
+            {
+
+                return BadRequest(guestValidationException.InnerException);
+            }
+            catch (GuestDependencyValidationException guestDependencyValidationException)
+                when (guestDependencyValidationException.InnerException is AlreadyExistsGuestException)
+            {
+                return Conflict(guestDependencyValidationException.InnerException);
+            }
+            catch (GuestDependencyException guestDependencyException)
+            {
+                return InternalServerError(guestDependencyException.InnerException);
+            }
+            catch (GuestServiceException guestServiceException)
+            {
+                return InternalServerError(guestServiceException.InnerException);
+            }
+        }
+    }
+}
