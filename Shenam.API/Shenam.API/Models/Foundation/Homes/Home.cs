89dc9dfdcb3dd2b21b339b8546d3aefa92a13688
Nodirbek Nasrullayev <nasrullayevnodirbek1535@gmail.com>
2025-11-08 14:21:56 +0500

    DATA: Add Homes Model

---
+++ Shenam.API/Shenam.API/Models/Foundation/Homes/Home.cs
@@ -0,0 +1,24 @@
+﻿//===============================================================
+//NODIRBEKNING MOHIRDEV PLATFORMASIDA ORGANGAN API SINOV LOYIHASI
+//===============================================================
+
+using System;
+
+namespace Shenam.API.Models.Foundation.Homes
+{
+    public class Home
+    {
+        public Guid Id { get; set; }
+        public Guid HostId { get; set; }
+        public string Address { get; set; }
+        public string AdditionalInfo {  get; set; }
+        public bool IsVacant { get; set; }
+        public int NumberOfBedrooms {  get; set; }
+        public int NumberOfBathrooms {  get; set; }
+        public double Area {  get; set; }
+        public bool IsPAllowed { get; set; }
+        public TypeHome HomeType { get; set; }
+        public decimal Price {  get; set; }
+        public bool IsShared {  get; set; }
+    }
+}
