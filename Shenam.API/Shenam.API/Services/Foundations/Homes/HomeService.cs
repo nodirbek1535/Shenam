4b619f59ee52d90b1cc58b64b4e1d25976730b92
Nodirbek Nasrullayev <nasrullayevnodirbek1535@gmail.com>
2025-12-22 13:13:36 +0500

    ShouldAddHomeAsync -> PASS

---
+++ Shenam.API/Shenam.API/Services/Foundations/Homes/HomeService.cs
@@ -0,0 +1,22 @@
+﻿//===============================================================
+//NODIRBEKNING MOHIRDEV PLATFORMASIDA ORGANGAN API SINOV LOYIHASI
+//===============================================================
+
+using Shenam.API.Brokers.Storages;
+using Shenam.API.Models.Foundation.Homes;
+using System;
+using System.Threading.Tasks;
+
+namespace Shenam.API.Services.Foundations.Homes
+{
+    public partial class HomeService:IHomeService
+    {
+        private readonly IStorageBroker storageBroker;
+
+        public HomeService(IStorageBroker storageBroker) => 
+            this.storageBroker = storageBroker;
+
+        public ValueTask<Home> AddHomeAsync(Home home) =>
+            throw new NotImplementedException();
+    }
+}
---
+++ Shenam.API/Shenam.Api.Tests.Unit/Services/Foundations/Homes/HomeServiceTests.cs
@@ -0,0 +1,32 @@
+﻿//===============================================================
+//NODIRBEKNING MOHIRDEV PLATFORMASIDA ORGANGAN API SINOV LOYIHASI
+//===============================================================
+
+using Moq;
+using Shenam.API.Brokers.Storages;
+using Shenam.API.Models.Foundation.Homes;
+using Shenam.API.Services.Foundations.Homes;
+using Tynamix.ObjectFiller;
+
+namespace Shenam.Api.Tests.Unit.Services.Foundations.Homes
+{
+    public partial class HomeServiceTests
+    {
+        private readonly Mock<IStorageBroker> storageBrokerMock;
+        private readonly IHomeService homeService;
+
+        public HomeServiceTests()
+        {
+            this.storageBrokerMock = new Mock<IStorageBroker>();
+
+            this.homeService = new HomeService(
+                storageBroker: this.storageBrokerMock.Object);
+        }
+
+        private static Home CreateRandomHome() =>
+            CreateHomeFiller().Create();
+
+        private static Filler<Home> CreateHomeFiller() =>
+            new Filler<Home>();
+    }
+}
