57ae16e507e6311dc78147ee1f4b06531b6d8f10
Nodirbek Nasrullayev <nasrullayevnodirbek1535@gmail.com>
2025-12-29 11:53:36 +0500

    ShouldThrowValidationExceptionOnAddIfHomeRequestIsInvalidAndLogItAsync -> FAIL

---
+++ Shenam.API/Shenam.API/Services/Foundations/HomeRequests/HomeRequestService.Exceptions.cs
@@ -0,0 +1,40 @@
+﻿//===============================================================
+//NODIRBEKNING MOHIRDEV PLATFORMASIDA ORGANGAN API SINOV LOYIHASI
+//===============================================================
+
+using Shenam.API.Models.Foundation.HomeRequests;
+using Shenam.API.Models.Foundation.HomeRequests.Exceptions;
+using System;
+using System.Threading.Tasks;
+using Xeptions;
+
+namespace Shenam.API.Services.Foundations.HomeRequests
+{
+    public partial class HomeRequestService
+    {
+
+        private delegate ValueTask<HomeRequest> ReturnningHomeRequestFunction();
+
+        private async ValueTask<HomeRequest> TryCatch(ReturnningHomeRequestFunction returnningHomeRequestFunction)
+        {
+            try
+            {
+                return await returnningHomeRequestFunction();
+            }
+            catch (NullHomeRequestException nullHomeRequestException)
+            {
+                throw CreateAndLogValidationException(nullHomeRequestException);
+            }
+        }
+
+        private HomeRequestValidationException CreateAndLogValidationException(Xeption exception)
+        {
+            var homeRequestValidationException =
+                new HomeRequestValidationException(exception);
+
+            this.loggingBroker.LogError(homeRequestValidationException);
+
+            return homeRequestValidationException;
+        }
+    }
+}
