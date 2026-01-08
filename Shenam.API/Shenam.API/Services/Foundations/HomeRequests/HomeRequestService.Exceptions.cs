fd7ba1b97836acf4b1f05713cd6c1c8afd275501
Nodirbek Nasrullayev <nasrullayevnodirbek1535@gmail.com>
2026-01-08 13:53:20 +0500

    ShouldThrowDependencyValidationOnAddIfDuplicateKeyErrorOccursAndLogItAsync -> PASS

--- Shenam.API/Shenam.API/Services/Foundations/HomeRequests/HomeRequestService.Exceptions.cs
+++ Shenam.API/Shenam.API/Services/Foundations/HomeRequests/HomeRequestService.Exceptions.cs
@@ -2,9 +2,11 @@
 //NODIRBEKNING MOHIRDEV PLATFORMASIDA ORGANGAN API SINOV LOYIHASI
 //===============================================================
 
+using EFxceptions.Models.Exceptions;
 using Microsoft.Data.SqlClient;
 using Shenam.API.Models.Foundation.HomeRequests;
 using Shenam.API.Models.Foundation.HomeRequests.Exceptions;
+using System.Net.NetworkInformation;
 using System.Threading.Tasks;
 using Xeptions;
 
@@ -25,14 +27,21 @@ namespace Shenam.API.Services.Foundations.HomeRequests
             {
                 throw CreateAndLogValidationException(nullHomeRequestException);
             }
-            catch(InvalidHomeRequestException invalidHomeRequestException)
+            catch (InvalidHomeRequestException invalidHomeRequestException)
             {
                 throw CreateAndLogValidationException(invalidHomeRequestException);
             }
-            catch(SqlException sqlException)
+            catch (SqlException sqlException)
             {
                 var failedHomeRequestStorageException = new FailedHomeRequestStorageException(sqlException);
                 throw CreateAndLogCriticalDependencyException(failedHomeRequestStorageException);
+            }
+            catch (DuplicateKeyException duplicateKeyException)
+            {
+                var alreadyExistsHomeRequestException =
+                    new AlreadyExistsHomeRequestException(duplicateKeyException);
+
+                throw CreateAndLogDependencyValidationException(alreadyExistsHomeRequestException);
             }
         }
 
@@ -54,6 +63,16 @@ namespace Shenam.API.Services.Foundations.HomeRequests
             this.loggingBroker.LogCritical(homeRequestDependencyException);
 
             return homeRequestDependencyException;
+        }
+
+        private HomeRequestDependencyValidationException CreateAndLogDependencyValidationException(Xeption exception)
+        {
+            var homeRequestDependencyValidationException =
+                new HomeRequestDependencyValidationException(exception);
+
+            this.loggingBroker.LogError(homeRequestDependencyValidationException);
+
+            return homeRequestDependencyValidationException;
         }
     }
 }
