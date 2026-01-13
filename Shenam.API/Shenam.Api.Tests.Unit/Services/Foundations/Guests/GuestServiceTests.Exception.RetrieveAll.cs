c97d5841fb9086a41a1f48db876f2a5479ebe094
Nodirbek Nasrullayev <nasrullayevnodirbek1535@gmail.com>
2026-01-14 00:28:24 +0500

    ShouldThrowCriticalDependencyExceptionOnRetrieveAllIfSqlErrorOccursAndLogIt -> FAIL

---
+++ Shenam.API/Shenam.Api.Tests.Unit/Services/Foundations/Guests/GuestServiceTests.Exception.RetrieveAll.cs
@@ -0,0 +1,56 @@
+﻿//===============================================================
+//NODIRBEKNING MOHIRDEV PLATFORMASIDA ORGANGAN API SINOV LOYIHASI
+//===============================================================
+
+using FluentAssertions;
+using Microsoft.Data.SqlClient;
+using Moq;
+using Shenam.API.Models.Foundation.Guests.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shenam.Api.Tests.Unit.Services.Foundations.Guests
+{
+    public partial class GuestServiceTests
+    {
+        [Fact]
+        public void ShouldThrowCriticalDependencyExceptionOnRetrieveAllIfSqlErrorOccursAndLogIt()
+        {
+            // given
+            SqlException sqlException = GetSqlError();
+
+            var failedStorageException =
+                new FailedGuestStorageException(sqlException);
+
+            var expectedDependencyException =
+                new GuestDependencyException(failedStorageException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectAllGuests()).Throws(sqlException);
+
+            // when
+            Action retrieveAllGuestsAction = () =>
+                this.guestService.RetrieveAllGuests();
+
+            // then
+            GuestDependencyException actualException =
+                Assert.Throws<GuestDependencyException>(retrieveAllGuestsAction);
+
+            actualException.Should().BeEquivalentTo(expectedDependencyException);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectAllGuests(), Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogCritical(It.Is(SameExceptionAs(
+                    expectedDependencyException))), Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+
+    }
+}
