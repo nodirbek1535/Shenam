0261735e5ba150a9a3a0f55a08e0741af40cede9
Nodirbek Nasrullayev <nasrullayevnodirbek1535@gmail.com>
2026-01-17 23:07:22 +0500

    ShouldThrowValidationExceptionOnRemoveIfGuestDoesNotExistAndLogItAsync -> PASS

---
+++ Shenam.API/Shenam.Api.Tests.Unit/Services/Foundations/Guests/GuestServiceTests.Validations.Remove.cs
@@ -0,0 +1,58 @@
+﻿//===============================================================
+//NODIRBEKNING MOHIRDEV PLATFORMASIDA ORGANGAN API SINOV LOYIHASI
+//===============================================================
+
+using Moq;
+using Shenam.API.Models.Foundation.Guests;
+using Shenam.API.Models.Foundation.Guests.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shenam.Api.Tests.Unit.Services.Foundations.Guests
+{
+    public partial class GuestServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnRemoveIfGuestDoesNotExistAndLogItAsync()
+        {
+            // given
+            Guid randomGuestId = Guid.NewGuid();
+            Guid inputGuestId = randomGuestId;
+            Guest noGuest = null;
+
+            var notFoundGuestException =
+                new NotFoundGuestException(inputGuestId);
+
+            var expectedGuestValidationException =
+                new GuestValidationException(notFoundGuestException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectGuestByIdAsync(inputGuestId))
+                    .ReturnsAsync(noGuest);
+
+            // when
+            ValueTask<Guest> removeGuestTask =
+                this.guestService.RemoveGuestByIdAsync(inputGuestId);
+
+            // then
+            await Assert.ThrowsAsync<GuestValidationException>(
+                () => removeGuestTask.AsTask());
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectGuestByIdAsync(inputGuestId),
+                Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedGuestValidationException))),
+                Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+
+    }
+}
