59e49e1735d83f917ab4b4045389f38337d34a84
Nodirbek Nasrullayev <nasrullayevnodirbek1535@gmail.com>
2026-01-18 15:30:00 +0500

    ShouldThrowValidationExceptionOnRetrieveByIdIfIdIsInvalidAndLogItAsync -> PASS

---
+++ Shenam.API/Shenam.Api.Tests.Unit/Services/Foundations/Homes/HomeServiceTests.Validatiuons.Retrieve.cs
@@ -0,0 +1,56 @@
+﻿//===============================================================
+//NODIRBEKNING MOHIRDEV PLATFORMASIDA ORGANGAN API SINOV LOYIHASI
+//===============================================================
+
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Moq;
+using Shenam.API.Models.Foundation.Homes;
+using Shenam.API.Models.Foundation.Homes.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shenam.Api.Tests.Unit.Services.Foundations.Homes
+{
+    public partial class HomeServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnRetrieveByIdIfIdIsInvalidAndLogItAsync()
+        {
+            //given
+            Guid invalidHomeId = Guid.Empty;
+
+            var invalidHomeException =
+                new InvalidHomeException();
+
+            invalidHomeException.AddData(
+                key: nameof(Home.Id),
+                values: "Id is required");
+
+            var expectedHomeValidationException =
+                new HomeValidationException(invalidHomeException);
+
+            //when
+            ValueTask<Home> retrieveHomeById =
+                this.homeService.RetrieveHomeByIdAsync(invalidHomeId);
+
+            //then
+            await Assert.ThrowsAsync<HomeValidationException>(() =>
+                retrieveHomeById.AsTask());
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedHomeValidationException))),
+                Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectHomeByIdAsync(It.IsAny<Guid>()),
+                Times.Never);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
