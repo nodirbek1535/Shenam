e843cea99c2a67b68c37248278554f310c6c5e9e
Nodirbek Nasrullayev <nasrullayevnodirbek1535@gmail.com>
2026-01-18 16:25:59 +0500

    ShouldRetrieveHomeByIdAsync -> PASS

---
+++ Shenam.API/Shenam.Api.Tests.Unit/Services/Foundations/Homes/HomeServiceTests.Logic.Retrieve.cs
@@ -0,0 +1,44 @@
+﻿//===============================================================
+//NODIRBEKNING MOHIRDEV PLATFORMASIDA ORGANGAN API SINOV LOYIHASI
+//===============================================================
+
+using FluentAssertions;
+using Moq;
+using Shenam.API.Models.Foundation.Homes;
+
+namespace Shenam.Api.Tests.Unit.Services.Foundations.Homes
+{
+    public partial class HomeServiceTests
+    {
+        [Fact]
+        public async Task ShouldRetrieveHomeByIdAsync()
+        {
+            // given
+            Guid randomHomeId = Guid.NewGuid();
+            Guid inputHomeId = randomHomeId;
+
+            Home randomHome = CreateRandomHome();
+            Home persistedHome = randomHome;
+            Home expectedHome = persistedHome;
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectHomeByIdAsync(inputHomeId))
+                    .ReturnsAsync(persistedHome);
+
+            // when
+            Home actualHome =
+                await this.homeService.RetrieveHomeByIdAsync(inputHomeId);
+
+            // then
+            actualHome.Should().BeEquivalentTo(expectedHome);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectHomeByIdAsync(inputHomeId),
+                    Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+
+        }
+    }
+}
