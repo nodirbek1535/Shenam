d0dd2fe61ec5619695c8addff64f117cbaf819a6
Nodirbek Nasrullayev <nasrullayevnodirbek1535@gmail.com>
2026-01-18 14:10:27 +0500

    SERVICES: Add RetrieveHomeByIdAsync

---
+++ Shenam.API/Shenam.API/Models/Foundation/Homes/Exceptions/NotFoundHomeException.cs
@@ -0,0 +1,16 @@
+﻿//===============================================================
+//NODIRBEKNING MOHIRDEV PLATFORMASIDA ORGANGAN API SINOV LOYIHASI
+//===============================================================
+
+using System;
+using Xeptions;
+
+namespace Shenam.API.Models.Foundation.Homes.Exceptions
+{
+    public class NotFoundHomeException:Xeption
+    {
+        public NotFoundHomeException(Guid homeId)
+            : base(message: $"Home with id: {homeId} is not found")
+        { }
+    }
+}
--- Shenam.API/Shenam.API/Services/Foundations/Homes/HomeService.Validations.cs
+++ Shenam.API/Shenam.API/Services/Foundations/Homes/HomeService.Validations.cs
@@ -34,6 +34,19 @@ namespace Shenam.API.Services.Foundations.Homes
             }
         }
 
+        private void ValidateHomeId(Guid homeId)
+        {
+            Validate(
+                (Rule: IsInvalid(homeId), Parameter: nameof(Home.Id)));
+        }
+
+        private void ValidateStorageHome(Home maybeHome, Guid homeId)
+        {
+            if (maybeHome is null)
+            {
+                throw new NotFoundHomeException(homeId);
+            }
+        }
         private static dynamic IsInvalid(Guid id) => new
         {
             Condition = id == Guid.Empty,
