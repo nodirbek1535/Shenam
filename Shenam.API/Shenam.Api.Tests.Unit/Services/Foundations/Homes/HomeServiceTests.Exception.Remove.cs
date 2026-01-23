b99aaeab0321f1428aec1f703adccb82733d562f
Nodirbek Nasrullayev <nasrullayevnodirbek1535@gmail.com>
2026-01-23 18:41:38 +0500

    ShouldThrowCriticalDependencyExceptionOnRemoveIfSqlErrorOccursAndLogItAsync -> PASS

---
+++ Shenam.API/Shenam.Api.Tests.Unit/Services/Foundations/Homes/HomeServiceTests.Exception.Remove.cs
@@ -0,0 +1,57 @@
+﻿//===============================================================
+//NODIRBEKNING MOHIRDEV PLATFORMASIDA ORGANGAN API SINOV LOYIHASI
+//===============================================================
+
+using Microsoft.Data.SqlClient;
+using Moq;
+using Shenam.API.Models.Foundation.Homes;
+using Shenam.API.Models.Foundation.Homes.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shenam.Api.Tests.Unit.Services.Foundations.Homes
+{
+    public  partial class HomeServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowCriticalDependencyExceptionOnRemoveIfSqlErrorOccursAndLogItAsync()
+        {
+            //given
+            Guid someHomeId = Guid.NewGuid();
+            SqlException sqlException = GetSqlError();
+
+            var failedHomeStorageException = 
+                new FailedHomeStorageException(sqlException);
+
+            var expectedHomeDependencyException =
+                new HomeDependencyException(failedHomeStorageException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectHomeByIdAsync(someHomeId))
+                    .ThrowsAsync(sqlException);
+
+            //when
+            ValueTask<Home> removeHomeTask =
+                this.homeService.RemoveHomeByIdAsync(someHomeId);
+
+            //then
+            await Assert.ThrowsAsync<HomeDependencyException>(() =>
+                removeHomeTask.AsTask());
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogCritical(It.Is(SameExceptionAs(
+                    expectedHomeDependencyException))),
+                        Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectHomeByIdAsync(someHomeId),
+                    Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
