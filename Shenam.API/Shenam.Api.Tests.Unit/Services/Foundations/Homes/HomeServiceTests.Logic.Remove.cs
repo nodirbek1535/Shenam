4b255e4dc89535dfe3084f3e4cdcaa6a28b1b840
Nodirbek Nasrullayev <nasrullayevnodirbek1535@gmail.com>
2026-01-23 18:32:54 +0500

    ShouldThrowRemoveHomeByIdAsync -> PASS

---
+++ Shenam.API/Shenam.Api.Tests.Unit/Services/Foundations/Homes/HomeServiceTests.Logic.Remove.cs
@@ -0,0 +1,54 @@
+﻿//===============================================================
+//NODIRBEKNING MOHIRDEV PLATFORMASIDA ORGANGAN API SINOV LOYIHASI
+//===============================================================
+
+using FluentAssertions;
+using Moq;
+using Shenam.API.Models.Foundation.Homes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shenam.Api.Tests.Unit.Services.Foundations.Homes
+{
+    public partial class HomeServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowRemoveHomeByIdAsync()
+        {
+            //given
+            Home randomHome = CreateRandomHome();
+            Home storedHome = randomHome;
+            Home deletedHome = storedHome;
+            Guid homeId = storedHome.Id;
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectHomeByIdAsync(homeId))
+                    .ReturnsAsync(storedHome);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.DeleteHomeAsync(storedHome))
+                    .ReturnsAsync(deletedHome);
+
+            //when
+            Home actualHome =
+                await this.homeService.RemoveHomeByIdAsync(homeId);
+
+            //then
+            actualHome.Should().BeEquivalentTo(deletedHome);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectHomeByIdAsync(homeId),
+                    Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.DeleteHomeAsync(storedHome),
+                    Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
