3a259a2a4c1bafc39d04e392dca8a3edce100700
Nodirbek Nasrullayev <nasrullayevnodirbek1535@gmail.com>
2026-01-28 18:37:15 +0500

    CONTROLLERS: Add PutHostEntityAsync

--- Shenam.API/Shenam.API/Controllers/HostEntityController.cs
+++ Shenam.API/Shenam.API/Controllers/HostEntityController.cs
@@ -102,5 +102,39 @@ namespace Shenam.API.Controllers
                     guestServiceException.InnerException);
             }
         }
+
+        [HttpPut]
+        public async ValueTask<ActionResult<HostEntity>> PutHostEntityAsync(HostEntity hostEntity)
+        {
+            try
+            {
+                HostEntity modifiedHostEntity =
+                    await this.hostEntityService.ModifyHostEntityAsync(hostEntity);
+
+                return Ok(modifiedHostEntity);
+            }
+            catch (HostEntityValidationException hostEntityValidationException)
+            {
+                return BadRequest(hostEntityValidationException.InnerException);
+            }
+            catch (HostEntityDependencyValidationException hostEntityDependencyValidationException)
+                when (hostEntityDependencyValidationException.InnerException is LockedHostEntityException)
+            {
+                return Locked(hostEntityDependencyValidationException.InnerException);
+            }
+            catch (HostEntityDependencyValidationException hostEntityDependencyValidationException)
+            {
+                return BadRequest(hostEntityDependencyValidationException.InnerException);
+            }
+            catch (HostEntityDependencyException hostEntityDependencyException)
+            {
+                return InternalServerError(hostEntityDependencyException.InnerException);
+            }
+            catch (HostEntityServiceException hostEntityServiceException)
+            {
+                return InternalServerError(hostEntityServiceException.InnerException);
+            }
+
+        }
     }
 }
