0ba4943bb10fd9ad3b6795100e7afb8c01bea675
Nodirbek Nasrullayev <nasrullayevnodirbek1535@gmail.com>
2026-01-31 00:28:57 +0500

    ShouldThrowValidationExceptionOnRemoveIfHomeRequestDoesNotExistAndLogItAsync -> PASS

---
+++ Shenam.API/Shenam.Api.Tests.Unit/Services/Foundations/HomeRequests/HomeRequestServiceTests.Validations.Remove.cs
@@ -0,0 +1,57 @@
+﻿//===============================================================
+//NODIRBEKNING MOHIRDEV PLATFORMASIDA ORGANGAN API SINOV LOYIHASI
+//===============================================================
+
+using Moq;
+using Shenam.API.Models.Foundation.HomeRequests;
+using Shenam.API.Models.Foundation.HomeRequests.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shenam.Api.Tests.Unit.Services.Foundations.HomeRequests
+{
+    public partial class HomeRequestServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnRemoveIfHomeRequestDoesNotExistAndLogItAsync()
+        {
+            // given
+            Guid randomHomeRequestId = Guid.NewGuid();
+            Guid inputHomeRequestId = randomHomeRequestId;
+            HomeRequest noHomeRequest = null;
+
+            var notFoundHomeRequestException =
+                new NotFoundHomeRequestException(inputHomeRequestId);
+
+            var expectedHomeRequestValidationException =
+                new HomeRequestValidationException(notFoundHomeRequestException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectHomeRequestByIdAsync(inputHomeRequestId))
+                    .ReturnsAsync(noHomeRequest);
+
+            // when
+            ValueTask<HomeRequest> removeHomeRequestTask =
+                this.homeRequestService.RemoveHomeRequestByIdAsync(inputHomeRequestId);
+
+            // then
+            await Assert.ThrowsAsync<HomeRequestValidationException>(() =>
+                removeHomeRequestTask.AsTask());
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectHomeRequestByIdAsync(inputHomeRequestId),
+                Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedHomeRequestValidationException))),
+                Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
